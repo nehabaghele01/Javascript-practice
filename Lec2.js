d0338157332b6af63713b914e1100635d8052951
Neha Baghele <167019723+nehabaghele01@users.noreply.github.com>
2025-02-26 22:21:50 +0530

    Add files via upload

---
+++ Lec2.js
@@ -0,0 +1,50 @@
+// OPEARATORS -> USED TO PERFORM SOME OPEARTION ON DATA
+
+// Unary operator -> modulus exponentation increment decrement
+
+// 1. Arithmetic operation -> + - * /
+
+// let a = 5;
+// let b = 2;
+// console.log("a = ", a, " &  b = ",b);
+// a = a-1; //a--
+// console.log("a = ",a);//4
+// b--;
+// console.log("b = ", b);//1
+
+// 2. Assignment operators -> = += -= *=  %= **=
+// += -> a = a+4 -> a+=4
+
+// let a = 5;
+// let b = 2;
+
+// a %= 4; // a= a % 4; -> give remainder
+// console.log("a = ", a);//1
+
+//3. Comparison operator-> == >= <= < > 
+
+// let a = 5;
+// let b = 2;
+
+// console.log("5==2",a==b);// false
+
+// let a = 5; //number
+// let b = "5"; //string -> number
+
+// // console.log("a == b" ,a == b);//true
+// console.log("a != b",a!=b);//false
+// console.log("5 <=5", a<=b); //true
+
+// let a = 6;
+// let b = 5;
+
+// console.log("cond1 && cond2", a<b && a===6);//false
+
+//4. LOGICAL OPERATOR
+
+// LOGICAL AND &&,LOGICAL OR ||, LOGICAL NOT !
+
+// let a = 6;
+// let b = 5;
+// console.log("!(6<5) = ", !(a === 6)); //false
+
