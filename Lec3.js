9756fcc8ebd903b71d633994bdd6e54e0b16ecb7
Neha Baghele <167019723+nehabaghele01@users.noreply.github.com>
2025-02-27 23:49:33 +0530

    Add files via upload

---
+++ Lec3.js
@@ -0,0 +1,87 @@
+// CONDITIONAL STATEMENTS -> TO IMPLEMENT SOME CONDITION IN THE CODE
+
+// IF
+//EX->
+// let color;
+// if(condition){
+//  code
+// }
+
+//EX->
+// let age = 18;
+
+// if(age>=18){
+//   console.log("you can vote");
+// }
+// if(age<18){
+//   console.log("you cannot vote");
+// }
+
+//EX->
+// let mode = "light";
+// let color;
+
+// if(mode=="dark"){
+//   color="black";
+
+// }
+// else{
+//   color="white";
+// }
+// console.log(color);
+
+// ELSE-IF STATEMENT-> multiple conditions
+// if(cond){
+//   code
+// }
+// else if(cond){
+//   code
+// }
+// else{
+//   code
+// }
+
+//EX->
+// let mode = "silver";
+// let color;
+
+// if (mode === "dark") {
+//   color = "black";
+// } else if (mode === "blue") {
+//   color = "blue;";
+// } else if (mode === "pink") {
+//   color = "pink";
+// } else {
+//   color = "white";
+// }
+// console.log(color);
+
+//EX->
+// let mode = "light";
+// let color;
+
+// if(mode==="dark") {
+//   color="black";
+
+// }
+// else{
+//   color="white";
+// }
+// console.log(color);
+
+
+// Ternary operator
+
+// condition?true output: false output
+
+let age = 25;
+
+let result = age>=18?"adult":"not adult";
+console.log(result);
+
+alert("Hello!");
+let name = prompt("hello!");
+console.log(name);
+
+// practices
+
